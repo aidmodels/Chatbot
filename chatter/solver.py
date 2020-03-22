66878e904931482100c3f5e4da4018db5915cf83
Xiaozhe Yao <xiaozhe.yaoi@gmail.com>
2020-03-22 01:55:43 +0100

    init repo

--- chatter/solver.py
+++ chatter/solver.py
@@ -1,15 +1,16 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from mlpm.solver import Solver
-
+from datetime import datetime
 
 class ChatbotSolver(Solver):
     def __init__(self, toml_file=None):
         super().__init__(toml_file)
         # Do you Init Work here
-        self.chatbot = ChatBot('AID_CHATBOT',    logic_adapters=[
-            'chatterbot.logic.MathematicalEvaluation',
-            'chatterbot.logic.TimeLogicAdapter'
+        self.chatbot = ChatBot('AID_CHATBOT',
+            logic_adapters=[
+                'chatterbot.logic.MathematicalEvaluation',
+                'chatterbot.logic.TimeLogicAdapter'
         ])
         trainer = ChatterBotCorpusTrainer(self.chatbot)
         # Train the chatbot based on the english corpus
@@ -18,5 +19,6 @@ class ChatbotSolver(Solver):
 
     def infer(self, data):
         # if you need to get file uploaded, get the path from input_file_path in data
-        result = self.chatbot.get_response(data["input"])
-        return {"output": result}  # return a dict
+        result = self.chatbot.get_response(data["input"]).serialize()
+        result['created_at']=datetime.timestamp(result['created_at'])
+        return {"output": result}
